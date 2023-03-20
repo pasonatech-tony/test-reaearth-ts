9085ba705c08a094c58e00a226ac3ab5a9affd19
user <thoang@fab.pasona.tech>
2023-03-20 21:03:10 +0900

    click on re-earth and get location to ui

--- src/widgets/spaceid.ts
+++ src/widgets/spaceid.ts
@@ -1,14 +1,38 @@
 import html from "../../dist/web/spaceid/index.html?raw";
 import type { MouseEvent } from "../apiType";
 
+type Point = {
+  lng: number | undefined;
+  lat: number | undefined;
+  height: number | undefined;
+};
+
 (globalThis as any).reearth.ui.show(html, {
   width: 350,
   height: 100,
   extended: true,
 });
 
+const addPoint = (point: Point) => {
+  (globalThis as any).reearth.ui.postMessage(
+    JSON.stringify({ act: "addPointAction", payload: point })
+  );
+};
+
+// how to connect click on re-earth and show it on ui: not yet
+// todo1:
 (globalThis as any).reearth.on("click", (msg: MouseEvent) => {
   // console.log(msg);
-  console.log(msg);
-  console.log("hello global reearth");
+  const lng = msg.lng;
+  const lat = msg.lat;
+  const height = msg.height;
+
+  const point = {
+    lng,
+    lat,
+    height,
+  };
+
+  console.log("point be: ", point);
+  addPoint(point);
 });
--- web/components/spaceid/App.tsx
+++ web/components/spaceid/App.tsx
@@ -1,23 +1,50 @@
-import { useEffect, useState } from "react";
+import type { actHandles } from "@web/types";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
-interface ISS {
+export type ISS = {
   altitude: number;
   longitude: number;
   latitude: number;
-}
+};
+
+export type Point = {
+  lng: number | undefined;
+  lat: number | undefined;
+  height: number | undefined;
+};
 
 const App = () => {
   const URL = "https://api.wheretheiss.at/v1/satellites/25544";
   const [data, setData] = useState<ISS>();
+  const [point, setPoint] = useState<Point>();
   async function fetchData() {
     const response = await fetch(`${URL}`);
     const data = await response.json();
     setData(data);
   }
 
+  const addPointAction = useCallback((point: Point) => {
+    setPoint(point);
+  }, []);
+
+  const actHandles: actHandles = useMemo(() => {
+    return {
+      addPointAction,
+    };
+  }, [addPointAction]);
+
   useEffect(() => {
     fetchData();
-  }, []);
+    (globalThis as any).addEventListener("message", (msg: any) => {
+      if (msg.source !== (globalThis as any).parent) return;
+      try {
+        const data =
+          typeof msg.data === "string" ? JSON.parse(msg.data) : msg.data;
+        actHandles[data.act as keyof actHandles]?.(data.payload);
+        // eslint-disable-next-line no-empty
+      } catch (error) {}
+    });
+  }, [actHandles]);
 
   return (
     <>
@@ -28,6 +55,10 @@ const App = () => {
       <p>
         <button onClick={fetchData}>Update</button>
       </p>
+      <h1>Current location when click on re-earth</h1>
+      <p>Latitude: {point?.lat}</p>
+      <p>Longitude: {point?.lng}</p>
+      <p>Height: {point?.height} </p>
     </>
   );
 };
